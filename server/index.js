92b621f8e640c559a626713458f4f3625876e970
Harshvardhan <shreeharshpb11@gmail.com>
2024-05-03 10:48:03 +0530

    connecting with mongodb

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
+import dotenv from 'dotenv'
+
+dotenv.config()
+// connecting express with const app
 const app = express()
 
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log('MongoDB Connected')
+}).catch((error) => {
+    console.log(error)
+})
+
+
+
+// listening server on port 
 app.listen(1000, () => {
     console.log('Server is running on port 1000')
 })
 
+
+// basic get route
 app.get('/', (req, res) => {
     res.json({
         message: "Welcome to our API",
