95e409c21d763d9574ace1bb22e7721e223dba3d
Harshvardhan <shreeharshpb11@gmail.com>
2024-05-04 12:40:16 +0530

    create signin functionality add rest operator for password to get remove from doc

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -45,17 +45,19 @@ export const signin = async (req, res, next) => {
     try {
         const validUser = await User.findOne({ email })
         if (!validUser) {
-           return  next(errorHandler(404, 'User Not Found!! Check the Credentials'))
+            return next(errorHandler(404, 'User Not Found!! Check the Credentials'))
         }
 
         const validPassword = bcryptjs.compareSync(password, validUser.password)
         if (!validPassword) {
-           return  next(errorHandler(404, 'Invalid Password'))
+            return next(errorHandler(404, 'Invalid Password'))
         }
 
         // eslint-disable-next-line no-undef
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY)
-        res.status(200).cookie('access_token', token, { httpOnly: true }).json({ message: 'SingIn SuccessFully', access_token: token })
+        // eslint-disable-next-line no-unused-vars
+        const { password: pass, ...rest } = validUser._doc
+        res.status(200).cookie('access_token', token, { httpOnly: true }).json({ message: 'SingIn SuccessFully', access_token: token, ...rest })
     } catch (error) {
         next(error)
     }
