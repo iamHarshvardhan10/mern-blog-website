131046e05ca6fab0daffff118e21008e3149fea9
Harshvardhan <shreeharshpb11@gmail.com>
2024-05-10 10:41:07 +0530

    fix some styling in header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import { MdLightMode } from "react-icons/md";
 import { MdDarkMode } from "react-icons/md";
+import { FaRegUserCircle } from "react-icons/fa";
+import { IoIosLogOut } from "react-icons/io";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
@@ -50,7 +52,7 @@ const Header = () => {
         </Link>
       </ul>
 
-      <div className="flex items-center gap-[2rem]">
+      <div className="flex items-center gap-[2rem] relative">
         <button
           className="border p-2 rounded-full border-black"
           onClick={handleToggle}
@@ -66,15 +68,29 @@ const Header = () => {
             <img
               src={currentUser.imageUrl}
               alt=""
-              className="w-[40px] h-[40px] rounded-full"
+              className="w-[40px] h-[40px] rounded-full cursor-pointer"
               onClick={handleDropClick}
             />
             {isdropMenu ? (
-              <div>
-                <p>{currentUser?.userName}</p>
-                <p>{currentUser?.email}</p>
-                <Link to="/dashboard?tab=profile">Profile</Link>
-                <Link to="/">Logout</Link>
+              <div className="flex flex-col absolute top-[50px] right-0  bg-[#010A43] rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-80 border-4 border-gray-400">
+                <p className="text-md border-b-2 border-white  text-white  p-4 font-bold cursor-pointer hover:bg-[#010A43]">
+                  {currentUser?.userName}
+                </p>
+                <p className="text-md  border-b-2 border-white  text-white  p-4  font-bold cursor-pointer  hover:bg-[#010A43]">
+                  {currentUser?.email}
+                </p>
+                <Link
+                  to="/dashboard?tab=profile"
+                  className="flex items-center gap-4 text-md font-bold border-b-2 border-white  text-white  p-4 hover:bg-[#010A43]"
+                >
+                  Profile <FaRegUserCircle className="text-2xl" />
+                </Link>
+                <Link
+                  to="/"
+                  className="flex items-center gap-4 text-md font-bold  text-white  p-4 hover:bg-[#010A43]"
+                >
+                  Logout <IoIosLogOut className="text-2xl" />
+                </Link>
               </div>
             ) : null}
           </div>
